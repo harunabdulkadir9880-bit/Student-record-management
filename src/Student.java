65779291add158904d2de3f1d48201dabb2f708a
Harun Abdulkadir <harunabdulkadir9880@gmail.com>
2026-06-17 17:39:39 +0300

    Added student fields

--- src/Student.java
+++ src/Student.java
@@ -1,3 +1,10 @@
-public class Student {
+import java.io.Serializable;
+public class Student implements Serializable {
+
+    String studentID;
+    String name;
+    String department;
+    double gpa;
+
 
 }

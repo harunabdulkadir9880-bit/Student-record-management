2f3029c260616b1b6be7cf3b3bb966c0a554233d
Harun Abdulkadir <harunabdulkadir9880@gmail.com>
2026-06-17 22:02:34 +0300

    This class creates a backup of the text file using Buffered Streams Buffered streams are faster because they read/write chunks of data at once

--- src/BackupManager.java
+++ src/BackupManager.java
@@ -1,2 +1,9 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.Date;
 public class BackupManager {
+
 }

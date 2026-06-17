c695532db3bf745c5ca9f508feac8cfa60ff3499
Harun Abdulkadir <harunabdulkadir9880@gmail.com>
2026-06-17 22:20:04 +0300

    Folder and file paths used across the whole program

--- src/FileHelper.java
+++ src/FileHelper.java
@@ -3,5 +3,13 @@ import java.util.Date;
 
 public class FileHelper{
 
+    public static String dataFolder   = "data";
+    public static String backupFolder = "backup";
+    public static String textFile     = "data/students.txt";
+    public static String binaryFile   = "data/students.dat";
+    public static String objectFile   = "data/students.obj";
+    public static String backupFile   = "backup/students_backup.txt";
+
+
 
 }

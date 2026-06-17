c7101d02eb20d6a1b021dd60207878324086351a
Harun Abdulkadir <harunabdulkadir9880@gmail.com>
2026-06-17 17:45:06 +0300

    constructor used to create new student object

--- src/Student.java
+++ src/Student.java
@@ -6,5 +6,12 @@ public class Student implements Serializable {
     String department;
     double gpa;
 
+    public Student(String studentID, String name, String department, double gpa) {
+        this.studentID = studentID;
+        this.name = name;
+        this.department = department;
+        this.gpa = gpa;
+    }
+
 
 }

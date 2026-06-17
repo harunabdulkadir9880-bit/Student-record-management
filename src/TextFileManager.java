5969c3d8ab5206171ba81c38183bd04060ddc87c
Harun Abdulkadir <harunabdulkadir9880@gmail.com>
2026-06-17 18:28:36 +0300

    Add a new student to the text file

--- src/TextFileManager.java
+++ src/TextFileManager.java
@@ -58,4 +58,22 @@ public class TextFileManager {
         }
     }
 
+    public static void addStudent(Student newStudent) {
+        ArrayList<Student> list = loadAllStudents();
+
+
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).studentID.equals(newStudent.studentID)) {
+                System.out.println("A student with this ID already exists!");
+                return;
+            }
+        }
+
+        list.add(newStudent);
+        saveAllStudents(list);
+        System.out.println("Student added successfully (Text File).");
+    }
+
+
+
 }

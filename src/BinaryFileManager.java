ad5ceb2a66b558428ad97b6f341e1ce5752d8e34
Harun Abdulkadir <harunabdulkadir9880@gmail.com>
2026-06-17 21:23:58 +0300

    Search for a student by ID

--- src/BinaryFileManager.java
+++ src/BinaryFileManager.java
@@ -72,4 +72,21 @@ public class BinaryFileManager {
         saveAllStudents(list);
         System.out.println("Student added successfully (Binary File).");
     }
+    public static Student searchStudent(String id) {
+        ArrayList<Student> list = loadAllStudents();
+
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).studentID.equals(id)) {
+                return list.get(i);
+            }
+        }
+
+        return null;
+    }
+
+
+
+
+
+
 }

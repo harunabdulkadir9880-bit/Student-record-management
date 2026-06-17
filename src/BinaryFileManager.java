23e628cfc3239a36094d51ac88bd5b89c6abcd1b
Harun Abdulkadir <harunabdulkadir9880@gmail.com>
2026-06-17 21:05:13 +0300

    This class saves and loads students using a binary (.dat) file

--- src/BinaryFileManager.java
+++ src/BinaryFileManager.java
@@ -1,2 +1,9 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
 public class BinaryFileManager {
+
 }

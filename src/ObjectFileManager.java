66c1182ad9f0236f6c7faff119ce52d905304876
Harun Abdulkadir <harunabdulkadir9880@gmail.com>
2026-06-17 21:38:58 +0300

    This class saves and loads students as Java objects using serialization

--- src/ObjectFileManager.java
+++ src/ObjectFileManager.java
@@ -1,2 +1,10 @@
-public class ObjectFileManager {
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+public class ObjectFileManager{
+
+
 }

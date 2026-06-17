a758952ebf169beb9b6fff6adb54a767b877d525
Harun Abdulkadir <harunabdulkadir9880@gmail.com>
2026-06-17 22:38:21 +0300

    Set up all folders and files before anything else

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,13 @@ import java.util.Scanner;
 public class Main  {
     public static void main(String[] args) {
 
+        FileHelper.setupFiles();
+
+        Scanner scanner = new Scanner(System.in);
+        int choice = 0;
+
+        System.out.println("\nWelcome to the Student Management System!");
+
+
     }
 }
